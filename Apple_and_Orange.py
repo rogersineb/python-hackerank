24a432c90a557ba85dcb8eb02a30c499756d05bc
rogersineb <searcherba@gmail.com>
2021-09-30 23:59:29 -0300

    Correções em Apple_and_Orange

--- Apple_and_Orange.py
+++ Apple_and_Orange.py
@@ -5,22 +5,25 @@
 if __name__ == '__main__':
 
 	s_house = 7
-	t_house = 11
-	a_position_apple_tree = 5
-	b_position_orange_tree = 15
-	d_distance_falled_apple = [-2, 2, 1]
-	d_distance_falled_orange = [5, -6]
+	t_house = 10
+	a_apple_tree = 4
+	b_orange_tree = 12
+	apples = [2, 3, -4]
+	oranges = [3,-2, -4]
 	apple_in_house = 0
 	orange_in_house = 0
 
-	for index_apple in range(len(d_distance_falled_apple)):
-		d_distance_falled_apple[index_apple] += a_position_apple_tree
-		if d_distance_falled_apple[index_apple] >= s_house:
+	apple_d_min = s_house - a_apple_tree
+	apple_d_max = t_house - a_apple_tree
+	orange_d_min = s_house - b_orange_tree
+	orange_d_max = t_house - b_orange_tree
+	
+	for apple in apples:
+		if apple >= apple_d_min and apple <= apple_d_max:
 			apple_in_house += 1
 
-	for index_orange in range(len(d_distance_falled_orange)):
-		d_distance_falled_orange[index_orange] += b_position_orange_tree
-		if d_distance_falled_orange[index_orange] <= t_house:
+	for orange in oranges:
+		if orange <= orange_d_max and orange >= orange_d_min:
 			orange_in_house += 1
 
 	print(apple_in_house, orange_in_house)		
